b6a30fc253d5832c5c96adad3b3dc2e09855f606
karishma9142 <karishmacair47050@gmail.com>
2025-09-27 09:52:24 +0530

    fisrt htttp server

--- to-do-app/index.js
+++ to-do-app/index.js
@@ -1,17 +1,38 @@
 const express=require("express");
-const app =express();
+
+//*************  creating a http server  ***************//
+
+function calculateSum(n){
+    let ans=0;
+    for(let i=1;i<=n;i++){
+        ans+=i;
+    }
+    return ans;
+}
+const app = express();
+
+app.get("/",function(req,res){
+    const n=req.query.n;
+    const sum=calculateSum(n);
+    res.send("hii sum is : " + sum);
+})
+
+app.listen(3000);  // the clinic address (needs to be unique for every process )
+
+
+
 // router handlers
-app.get('/',function(req,res){
-    res.send("hello word");
-})
-app.post('/',function(req,res){
-    res.send("hello word from post endpoint");
-})
-app.get('/asb',function(req,res){
-    res.send("hello word from asb endpoint");
-})
-app.listen(3000, () => {
-    console.log("Server is running on http://localhost:3000");
-});
+// app.get('/',function(req,res){
+//     res.send("hello word");
+// })
+// app.post('/',function(req,res){
+//     res.send("hello word from post endpoint");
+// })
+// app.get('/asb',function(req,res){
+//     res.send("hello word from asb endpoint");
+// })
+// app.listen(3000, () => {
+//     console.log("Server is running on http://localhost:3000");
+// });
 
 // app.listen(3000);  // which port 
