7a395b4045d270275324a75d36e83c3057e18cf3
karishma9142 <karishmacair47050@gmail.com>
2025-10-03 10:23:38 +0530

    change

--- MidleWare/errorCnt.js
+++ MidleWare/errorCnt.js
@@ -6,8 +6,8 @@ app.use(function(req,res,res ,next){
     errorCnt=errorCnt+1;
 })
 app.get("/user" , function(req,res){
-    throw new Error("user not found");
-    res.status(200).json({ame : "Karishma"})
+    throw new Error("user not found")
+    res.status(200).json({name : "Karishma"})
 })
 app.post("/user" , function(req,res){
     res.status(200).json({msg : "created dummy account"})
