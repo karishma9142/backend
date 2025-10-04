fcc2ea7238d2bac8d2e747e8ccf93ba1f30755c9
karishma9142 <karishmacair47050@gmail.com>
2025-10-05 00:09:00 +0530

    week-5 class-1

--- fetch/script.js
+++ fetch/script.js
@@ -1,26 +1,26 @@
 const express = require ("express");
 const app = express();
-app.get("/add" , function(req,res){
-    let a = parseInt(req.query.a);
-    let b=parseInt(req.query.b);
+app.get("/add/:a/:b" , function(req,res){
+    let a = parseInt(req.params.a);
+    let b=parseInt(req.params.b);
     let ans=a+b;
     res.send("The sum is " + ans);
 })
-app.get("/multiply" , function(req,res){
-    let a = parseInt(req.query.a);
-    let b=parseInt(req.query.b);
+app.get("/multiply/:a/:b" , function(req,res){
+    let a = parseInt(req.params.a);
+    let b=parseInt(req.params.b);
     let ans=a*b;
     res.send("The multliplication is " + ans);
 })
-app.get("/divide" , function(req,res){
-    let a = parseInt(req.query.a);
-    let b=parseInt(req.query.b);
+app.get("/divide/:a/:b" , function(req,res){
+    let a = parseInt(req.params.a);
+    let b=parseInt(req.params.b);
     let ans=a/b;
     res.send("The division is " + ans);
 })
-app.get("/substract" , function(req,res){
-    let a = parseInt(req.query.a);
-    let b=parseInt(req.query.b);
+app.get("/substract/:a/:b" , function(req,res){
+    let a = parseInt(req.params.a);
+    let b=parseInt(req.params.b);
     let ans=a-b;
     res.send("The substcarion is " + ans);
 })
