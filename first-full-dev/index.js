c70b25d2d818ef5a990342bce389a1eadc4d4e51
karishma9142 <karishmacair47050@gmail.com>
2025-10-09 20:20:11 +0530

    fiirst full muliplicaation page

---
+++ first-full-dev/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.post("/divide" , function(req,res){
+    let a = parseInt(req.body.a);
+    let b = parseInt(req.body.b);
+    res.json({
+        answer : a*b,
+    })
+})
+app.listen(3000);
