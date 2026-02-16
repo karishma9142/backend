4e6e6a451cc9cbb66d896b5b1ca47c403b1971db
karishma9142 <karishmacair47050@gmail.com>
2026-02-16 17:32:39 +0530

    start learning next js

--- nextJs/first-class/app/signin/page.tsx
+++ nextJs/first-class/app/signin/page.tsx
@@ -2,6 +2,9 @@ export default function Signin(){
     return (
         <div>
             Sign in page
+            <input type="text" placeholder="Enter your name" />
+            <input type="text" placeholder="Enter your password"/>
+            <button>submit</button>
         </div>
     )
 }
