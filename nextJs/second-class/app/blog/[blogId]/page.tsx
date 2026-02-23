8a91e3aca9ee24ebaabd96a5f0059902ff11ed1f
karishma9142 <karishmacair47050@gmail.com>
2026-02-24 00:11:09 +0530

    .....

--- nextJs/second-class/app/blog/[blogId]/page.tsx
+++ nextJs/second-class/app/blog/[blogId]/page.tsx
@@ -1,3 +1,5 @@
+import axios from 'axios'
+
 interface BlogPageProps {
     params: Promise<{
       blogId: string;
@@ -6,10 +8,21 @@ interface BlogPageProps {
   
   export default async function BlogPage({ params }: BlogPageProps) {
     const { blogId } = await params;
+    const response = await axios.get(`https://jsonplaceholder.typicode.com/todos/${blogId}`);
+    const data = response.data;
   
     return (
       <div>
-        blog page {blogId}
+        blog page 
+        <br />
+        <br />
+        userId : {data.userId}
+        <br />
+        id : {data.id}
+        <br />
+        title : {data.title}
+        <br />
+        completed :  {data.completed ? "true" : "false"}
       </div>
     );
   }
